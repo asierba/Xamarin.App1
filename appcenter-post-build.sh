ff1f2e6c9ac3ec15efb9ddf483bd495279ecec84
Asier Barrenetxea <asier.barrenetxea@informa.com>
2018-06-12 16:12:31 +0100

    Update scripts

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,2 +1,13 @@
-cd UnitTests/
-dotnet test
+echo "Found Unit test projects:"
+find $APPCENTER_SOURCE_DIRECTORY-regex '.*UnitTests.*\.csproj' -exec echo {} \;
+echo
+echo "Building Unit test projects:"
+Find $APPCENTER_SOURCE_DIRECTORY -regex '.*UnitTests.*\.csproj' -exec msbuild {} \;
+echo
+echo "Compiledprojects to run Unit tests:"
+find $APPCENTER_SOURCE_DIRECTORY -regex '.*bin.*UnitTests.*\.dll' -exec echo {} \;
+echo
+echo "Running Unit tests:"
+find$APPCENTER_SOURCE_DIRECTORY -regex '.*bin.*UnitTests.*\.dll' -exec dotnet vstest --logger:trx {} \;
+ echo
+find $APPCENTER_SOURCE_DIRECTORY/Unittest/TestResults -name '*.trx' -exec cat {} \;
