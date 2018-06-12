008fdfa14d089f2a21ddd4c805b16f6507109ac1
Asier Barrenetxea <asier.barrenetxea@informa.com>
2018-06-12 15:21:11 +0100

    Initial commit

---
+++ App1/App1.Android/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace App1.Droid
+{
+    [Activity(Label = "App1", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+
+            AppCenter.Start("6c1abb80-7c0f-4e24-a577-cfa85d4e053d", typeof(Analytics), typeof(Crashes));
+        }
+    }
+}
+
---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AppCenter.Crashes;
+using Xamarin.Forms;
+
+namespace App1
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void Button_Clicked()
+        {
+
+        }
+
+		private void Button_OnClicked(object sender, EventArgs e)
+		{
+			throw new NotImplementedException("this is broken! :(");
+        }
+
+	    private void Error(object sender, EventArgs e)
+	    {
+	        try
+	        {
+	            throw new NotImplementedException("this is broken! :(");
+            }
+	        catch (Exception exception)
+	        {
+	            Crashes.TrackError(exception);
+	        }
+	    }
+
+
+	}
+}
